6296af244c3559d202a100c9e621b71861bf3db3
CY <cycleapple@gmail.com>
2026-01-06 10:44:54 +0800

    UserAgent 改為 Windows 11 Chrome

--- XIV on Mac/Launcher/LaunchController.swift
+++ XIV on Mac/Launcher/LaunchController.swift
@@ -298,7 +298,7 @@ class LaunchController: NSViewController, WKNavigationDelegate {
         loginPageWebView.navigationDelegate = self
 
         // 設置 Chrome-like UserAgent 提升 reCAPTCHA 信任度
-        loginPageWebView.customUserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+        loginPageWebView.customUserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
 
         // 設置透明背景
         loginPageWebView.setValue(false, forKey: "drawsBackground")
